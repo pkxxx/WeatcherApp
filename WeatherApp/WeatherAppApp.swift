849e5f7055920b891ec1417b8306a71efe03b2d4
Piotr Kowalski <pkowalski@alan-systems.com>
2023-04-11 13:49:05 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Piotr Kowalski on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
